0a5830cf7c81d3d62483ed79f7a5da20eeb92fef
Noufal Ibrahim <noufal@hamon.in>
2019-09-05 12:30:14 +0530

    Adds first cut of models

--- bookclub/model.py
+++ bookclub/model.py
@@ -1,5 +1,5 @@
 # from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import relationship
 from flask_sqlalchemy import SQLAlchemy
 
 from .app import app
@@ -11,11 +11,39 @@ class BookClubBase:
         name = self.name if hasattr(self, "name") else self.id
         return "<{}(name='{}'...)>".format(self.__class__.__name__, name)
 
-class User(db.Model):
+class User(db.Model, BookClubBase):
     __tablename__ = "bookclub_user"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
     pwhash = db.Column(db.String(256))
+    shelves = relationship('Shelf', backref='user')
+    reviews = relationship('Review', backref='user')
+
+class Shelf(db.Model, BookClubBase):
+    __tablename__ = "bookclub_shelf"
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('bookclub_user.id'))
+    name = db.Column(db.String(50))
+    books = relationship('Book', backref='shelf')
+
+class Book(db.Model, BookClubBase):
+    __tablename__ = "bookclub_book"
+    id = db.Column(db.Integer, primary_key=True)
+    slug = db.Column(db.String(20), unique=True)
+    shelf_id = db.Column(db.Integer, db.ForeignKey('bookclub_shelf.id'))
+    name = db.Column(db.String(50))
+    author = db.Column(db.String(70))
+    brief = db.Column(db.String(300))
+    
+class Review(db.Model, BookClubBase):
+    __tablename__ = "bookclub_review"
+    id = db.Column(db.Integer, primary_key = True)
+    date = db.Column(db.Date())
+    user_id = db.Column(db.Integer, db.ForeignKey('bookclub_user.id'))
+    book_id = db.Column(db.Integer, db.ForeignKey('bookclub_book.id'))
+    __table_args__ = (db.UniqueConstraint('user_id', 'book_id', name = '_user_book'),)
+    
+    
 
 
 
