c72f61e45bb5fd56ff1cb86ac77573795d7f2a3d
Noufal Ibrahim <noufal@hamon.in>
2019-09-05 10:54:19 +0530

    Adds auth

--- bookclub/app.py
+++ bookclub/app.py
@@ -1,11 +1,19 @@
-from flask import Flask
-from flask import jsonify
-
+from flask import Flask, jsonify
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
 app.config.from_object('config')
 
+auth = HTTPBasicAuth()
+
+@auth.verify_password
+def verify_password(username, password):
+    if username == 'noufal' and password == 'secret':
+        return True
+    else:
+        return False
 
 @app.route('/')
+@auth.login_required
 def index():
     return jsonify(dict(status='all good', data={}))
