e8ef739937753c6f28c3b2d38c8325219cf0bce4
Noufal Ibrahim <noufal@nibrahim.net.in>
2019-09-08 17:42:35 +0530

    Adds book list component

--- bc-frontend/src/app/app.module.ts
+++ bc-frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
